76999c7de158fa3384f84040ef3814bd6a9f82c0
smcmeans <sean.mcmeans@outlook.com>
2025-12-20 01:50:53 -0500

    Added closet

--- build_menu.gd
+++ build_menu.gd
@@ -6,8 +6,7 @@ signal request_tower_placement(tower_scene, cost)
 # Preload your tower scenes here
 var washing_machine_scene = preload("res://towers/WashingMachine.tscn")
 var dryer_scene = preload("res://towers/Dryer.tscn")
-
-@onready var menu_control = $Background
+var closet_scene = preload("res://towers/Closet.tscn")
 
 func _ready():
 	# Hide the menu when the game starts
@@ -15,8 +14,9 @@ func _ready():
 	
 	# Connect the button click
 	# (Adjust path if your button is deeper in the tree)
-	$Background/GridContainer/BtnWashingMachine.pressed.connect(_on_washer_clicked)
-	$Background/GridContainer/BtnDryer.pressed.connect(_on_dryer_clicked)
+	$GridContainer/BtnWashingMachine.pressed.connect(_on_washer_clicked)
+	$GridContainer/BtnDryer.pressed.connect(_on_dryer_clicked)
+	$GridContainer/BtnCloset.pressed.connect(_on_closet_clicked)
 
 func _input(event):
 	if event.is_action_pressed("toggle_build_menu"):
@@ -49,3 +49,13 @@ func _on_dryer_clicked():
 	toggle_menu()
 
 	emit_signal("request_tower_placement", dryer_scene, cost)
+
+func _on_closet_clicked():
+	var cost = 100
+	if GameData.money < cost:
+		print("Not enough money to build Dryer!")
+		return
+
+	toggle_menu()
+
+	emit_signal("request_tower_placement", closet_scene, cost)
