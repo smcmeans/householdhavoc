86d525a3c1b12d3adf08624af181d90f01318acd
Sean McMeans <sean.mcmeans@outlook.com>
2026-02-05 11:08:07 -0500

    Updated comments

--- money_label.gd
+++ money_label.gd
@@ -1,10 +1,10 @@
 extends Label
 
 func _ready():
-	# 1. Update text at start
+	# Update text at start
 	text = "Money: " + str(GameData.money)
 	
-	# 2. Listen for changes from the Bank
+	# Listen for changes from the Bank
 	GameData.money_changed.connect(_on_money_changed)
 
 func _on_money_changed(new_amount):
